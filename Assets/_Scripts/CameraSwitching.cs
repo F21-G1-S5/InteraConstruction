b693e1a7a5937b6cc3cb7f27684f00c1c0a74f78
Edgar <edgaradrian.mejiarazo@gmail.com>
2021-12-04 20:50:39 -0500

    Changing Cameras Logic

---
+++ Assets/_Scripts/CameraSwitching.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitching : MonoBehaviour
+{
+    public GameObject camCrane;
+    public GameObject camBulldozer;
+    public GameObject camScissorLift;
+    public GameObject MainCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Crane"))
+        {
+            Debug.Log("pressing Crane Camera");
+            camCrane.SetActive(true);
+            camBulldozer.SetActive(false);
+            camScissorLift.SetActive(false);
+            MainCamera.SetActive(false);
+        }
+        if (Input.GetButtonDown("Bulldozer"))
+        {
+            camCrane.SetActive(false);
+            camBulldozer.SetActive(true);
+            camScissorLift.SetActive(false);
+            MainCamera.SetActive(false);
+        }
+        if (Input.GetButtonDown("ScissorLift"))
+        {
+            camCrane.SetActive(false);
+            camBulldozer.SetActive(false);
+            camScissorLift.SetActive(true);
+            MainCamera.SetActive(false);
+        }
+        if (Input.GetButtonDown("MainCamera"))
+        {
+            camCrane.SetActive(false);
+            camBulldozer.SetActive(false);
+            camScissorLift.SetActive(false);
+            MainCamera.SetActive(true);
+        }
+
+    }
+}
